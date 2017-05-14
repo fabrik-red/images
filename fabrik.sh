37460a5ac88fb2f11c1bd52127f41731cda6ca4c
nbari <nbari@tequila.io>
2017-05-14 20:36:47 +0200

    208.67.222.222

--- fabrik.sh
+++ fabrik.sh
@@ -311,6 +311,7 @@ EOF
 # jail /etc/resolv.conf
 cat << EOF > /mnt/jails/base/etc/resolv.conf
 nameserver 84.200.70.40
+nameserver 208.67.222.222
 nameserver 4.2.2.2
 nameserver 2001:4860:4860::8888
 nameserver 2001:1608:10:25::1c04:b12f
